5a88972ac7d6f59cc9d3e029e92bf4656bdd4e25
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 13:30:01 -0400

    Update 13.pascal triangle as 2d array .java

--- Beginners Assignment/Math Algorthims/13.pascal triangle as 2d array .java
+++ Beginners Assignment/Math Algorthims/13.pascal triangle as 2d array .java
@@ -1,1 +1,17 @@
+import java.util.*;
+public class first {
 
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int dp[][]=new int[n+1][n+1];
+		for(int i=0;i<n+1;++i) {
+			for(int j=i;j>0;--j) {
+				if(i==j||j==0)dp[i][j]=1;
+				else dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+				System.out.print(dp[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
