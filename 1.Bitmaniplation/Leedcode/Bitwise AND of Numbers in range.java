c4023066eea030b85ae4c58f787f595908140896
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 12:52:37 -0400

    Update Bitwise AND of Numbers in range.java

--- 1.Bitmaniplation/Leedcode/Bitwise AND of Numbers in range.java
+++ 1.Bitmaniplation/Leedcode/Bitwise AND of Numbers in range.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int rangeBitwiseAnd(int l, int r) {
+        int count=0;
+        while(l!=r){
+            l>>=1;
+            r>>=1;
+            count++;
+        }
+        return l<<count;
+    }
+}
