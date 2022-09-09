b87124ef14055681038ccff3c4986013cdd497c8
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 13:20:04 -0400

    Update 10.given string is palindrome.java

--- Beginners Assignment/Math Algorthims/10.given string is palindrome.java
+++ Beginners Assignment/Math Algorthims/10.given string is palindrome.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class first {
+
+	public static void main(String ...args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.print(isString(s));
+		
+		
+	}public static boolean isString(String s) {
+		int i=0,j=s.length()-1;
+		while(i<j) {
+			if(s.charAt(i)!=s.charAt(j))return false;
+			i++;
+			j--;
+		}
+		return true;
+	}	
+}
