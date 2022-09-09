e3bee342661db3390e3e90387874925164b1e5cb
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 12:54:34 -0400

    Update 2.Given a number find if it's prime.java

--- 2.MathAlgorithm/Assignment/2.Given a number find if it's prime.java
+++ 2.MathAlgorithm/Assignment/2.Given a number find if it's prime.java
@@ -1,1 +1,20 @@
+import java.util.*;
+public class first {
 
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if((isprime(n))) {
+			System.out.print(true);
+		}else {
+			System.out.print(false);
+		}
+	}
+	public static boolean isprime(int a) {
+		for(int i=2;i<=Math.sqrt(a);i++) {
+			if(a%i==0)return false;
+		}
+		return true;
+	}
+
+}
