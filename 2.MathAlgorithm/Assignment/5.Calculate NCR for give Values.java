beb74e992db298c0e0c786a3ff3336a023b33090
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 12:55:49 -0400

    Update 5.Calculate NCR for give Values.java

--- 2.MathAlgorithm/Assignment/5.Calculate NCR for give Values.java
+++ 2.MathAlgorithm/Assignment/5.Calculate NCR for give Values.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public class first {
 
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int b=sc.nextInt();
+		int dp[]=new int[n+1];
+		for(int i=0;i<n+1;++i) {
+			for(int j=i;j>0;--j) {
+				if(i==j||j==0)dp[j]=1;
+				else dp[j]=dp[j-1]+dp[j];
+				System.out.print(dp[j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.print(dp[b]);
+	}
+}
