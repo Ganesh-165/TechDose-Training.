19d429b4f47bb450de1e007e568349da31ce9512
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 12:51:08 -0400

    Update 3.CountingBits.java

--- 1.Bitmaniplation/Leedcode/3.CountingBits.java
+++ 1.Bitmaniplation/Leedcode/3.CountingBits.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int[] countBits(int n) {
+        int ptr=0;
+        int res[]=new int[n+1];
+        for(int j=0;j<=n;j++){
+            int i=j;
+            int count=0;
+            while(i!=0){
+                i=(i&(i-1));
+                count++;
+            }
+            res[ptr++]=count;
+        }
+        return res;
+    }
+}
