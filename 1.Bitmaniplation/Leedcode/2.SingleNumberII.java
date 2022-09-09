c14d8e10105d023c6eccf064256d367599e8ea95
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 12:50:36 -0400

    Update 2.SingleNumberII.java

--- 1.Bitmaniplation/Leedcode/2.SingleNumberII.java
+++ 1.Bitmaniplation/Leedcode/2.SingleNumberII.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int singleNumber(int[] n) {
+        int ans=0;
+        for(int i=0;i<=31;i++){
+            int one=0;
+            for(int j=0;j<n.length;j++){
+                if(((n[j]>>i)&1)==1)one++;
+            }
+            one%=3;
+            if(one!=0)ans|=one<<i;
+        }
+        return ans;
+    }
+}
