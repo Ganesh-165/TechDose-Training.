273a3ec7cf93993da2bc539ac097a853688ebcbe
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 13:26:42 -0400

    Update 12.Find Factorial of given Number.java

--- Beginners Assignment/Math Algorthims/12.Find Factorial of given Number.java
+++ Beginners Assignment/Math Algorthims/12.Find Factorial of given Number.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+public class first {
+
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=1;
+		for(int i=1;i<=a;i++) {
+			b=b*i;
+		}
+		System.out.print(b);
+		
+		
+	}
+}
