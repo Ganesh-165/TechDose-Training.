cf466bc51ef6ef04372309951db4b7609b2017d6
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 12:51:40 -0400

    Update 4.HammingDistance.java

--- 1.Bitmaniplation/Leedcode/4.HammingDistance.java
+++ 1.Bitmaniplation/Leedcode/4.HammingDistance.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int hammingDistance(int x, int y){
+        x^=y;
+        int count=0;
+        while(x!=0){
+            x=(x&(x-1));
+            count++;
+        }
+        return count;
+    }
+}
