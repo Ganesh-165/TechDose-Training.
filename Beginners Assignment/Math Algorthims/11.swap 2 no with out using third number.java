56245a4edef69b3c19b467cf6767c1c162bf0eeb
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 13:22:36 -0400

    Update 11.swap 2 no with out using third number.java

--- Beginners Assignment/Math Algorthims/11.swap 2 no with out using third number.java
+++ Beginners Assignment/Math Algorthims/11.swap 2 no with out using third number.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+public class first {
+
+	public static void main(String ...args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		a^=b;
+		b^=a;
+		a^=b;
+		System.out.printf("%d %d",a,b);
+		
+		
+	}
+}
