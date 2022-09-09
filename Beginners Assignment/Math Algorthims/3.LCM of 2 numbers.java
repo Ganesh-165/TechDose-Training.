4c9cb4f1d04c644c336576135be09a5b991f1d37
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-09 13:16:26 -0400

    Update 3.LCM of 2 numbers.java

--- Beginners Assignment/Math Algorthims/3.LCM of 2 numbers.java
+++ Beginners Assignment/Math Algorthims/3.LCM of 2 numbers.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+public class first {
+
+	public static void main(String ...args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.print((n*b)/gcd(n,b));
+		
+	}public static int gcd(int a,int b) {
+		if(a==0)return b;
+		return gcd(b%a,a);
+	}
+	
+}
