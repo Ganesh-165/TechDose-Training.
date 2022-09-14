4c688e187892b8a93a53facccbcfd6f01f288d06
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-14 07:59:56 -0400

    Update 3.Insertiorn Sort.java

--- 3.Sorting/Assignment/3.Insertiorn Sort.java
+++ 3.Sorting/Assignment/3.Insertiorn Sort.java
@@ -1,1 +1,32 @@
-
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums.length == 0) return nums;
+        insertionSort(nums);
+        return nums;
+    }
+    private void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (nums[j] >= nums[j - 1]) break;
+                swap(nums, j, j - 1);
+            }
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();  
+        }
+        sortArray(nums);
+        for(int i=0;i<n;i++){
+          System.out.println(nums[i]);
+        }
+     }
+}
