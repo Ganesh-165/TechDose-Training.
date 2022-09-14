2f946e1cec9f6dfa92e9c830d468ac913e85d5ae
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-14 08:03:48 -0400

    Update 1.SelectionSort.java

--- 3.Sorting/Assignment/1.SelectionSort.java
+++ 3.Sorting/Assignment/1.SelectionSort.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public static int[] sortArray(int[] nums) {
+        if (nums.length == 0) return res;
+        selectionSort(nums);
+        return nums;
+    }
+    private static void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) minIndex = j;
+            }
+            if (minIndex != i) swap(nums, i, minIndex);
+        }
+    }
+    private static  void swap(int[] nums, int i, int j) {
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        sortArray(nums);
+        System.out.println(nums);
+    }
+}
