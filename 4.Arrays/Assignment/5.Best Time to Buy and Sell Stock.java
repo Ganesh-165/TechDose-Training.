1ffc710bd515c1ccf546c68a0d257c4548285993
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-09-24 09:23:30 -0400

    Update 5.Best Time to Buy and Sell Stock.java

--- 4.Arrays/Assignment/5.Best Time to Buy and Sell Stock.java
+++ 4.Arrays/Assignment/5.Best Time to Buy and Sell Stock.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public int maxProfit(int[] arr) {
+        int min=arr[0];int maximum=0;
+        for(int i=0;i<arr.length;i++){
+            min=Math.min(arr[i],min);
+            maximum=Math.max(arr[i]-min,maximum);
+        }
+        return maximum;
+    }
+}
