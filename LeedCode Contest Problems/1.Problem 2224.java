26a5d7d4a1273ce4ae42d2a31a1946710b35537c
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-10-14 08:46:07 -0400

    Update 1.Problem 2224.java

--- LeedCode Contest Problems/1.Problem 2224.java
+++ LeedCode Contest Problems/1.Problem 2224.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int convertTime(String str1, String str2) {
+        int currmin=Integer.parseInt(str1.substring(0,2))*60+Integer.parseInt(str1.substring(3));
+        int cormin=Integer.parseInt(str2.substring(0,2))*60+Integer.parseInt(str2.substring(3))-currmin;
+        int cov[]={1,5,15,60};
+        int count=0,i=3;
+        while(cormin>0){
+            if(cormin<cov[i])i--;
+            else{
+                cormin-=cov[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
