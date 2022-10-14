1ab62f47368fa7ff90485090abbf1cf68096293b
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-10-14 08:48:04 -0400

    Update 2.Problem 2231.java

--- LeedCode Contest Problems/2.Problem 2231.java
+++ LeedCode Contest Problems/2.Problem 2231.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int largestInteger(int n) {
+        char c[]=String.valueOf(n).toCharArray();
+        for(int i=0;i<c.length;i++){
+            for(int j=i+1;j<c.length;j++){
+                if(c[j]>c[i]&&(c[j]-c[i])%2==0){
+                    char a=c[i];
+                    c[i]=c[j];
+                    c[j]=a;
+                }
+            }
+        }
+        return Integer.parseInt(new String(c));
+    }
+}
