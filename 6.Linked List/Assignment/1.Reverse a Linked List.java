275d2717feda12e0e8090bdb78b7a086109acb43
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-10-26 09:13:49 -0400

    Update 1.Reverse a Linked List.java

--- 6.Linked List/Assignment/1.Reverse a Linked List.java
+++ 6.Linked List/Assignment/1.Reverse a Linked List.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode t=head,q=null,r=null;
+        while(t!=null){
+            r=q;
+            q=t;
+            t=t.next;
+            q.next=r;
+        }
+        return q;
+    }
+}
