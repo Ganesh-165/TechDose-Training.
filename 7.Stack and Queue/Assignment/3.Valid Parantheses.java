721e51ce14d132e876ce16718d3185428076ad6e
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-10-26 05:26:21 -0400

    Update 3.Valid Parantheses.java

--- 7.Stack and Queue/Assignment/3.Valid Parantheses.java
+++ 7.Stack and Queue/Assignment/3.Valid Parantheses.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> a=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!a.isEmpty()&&(a.peek()==c-1 ||a.peek()==c-2))a.pop();
+            else a.push(c);
+        }
+        if(a.isEmpty())return true;
+        else return false;
+    }
+}
