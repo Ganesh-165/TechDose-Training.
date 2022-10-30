1d716887f24502ab461053f8bd73371c9cd64e08
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-10-30 11:41:05 -0400

    Update 5.Largest Histogram in Rectangle.java

--- 7.Stack and Queue/Assignment/5.Largest Histogram in Rectangle.java
+++ 7.Stack and Queue/Assignment/5.Largest Histogram in Rectangle.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int largestRectangleArea(int[] h) {
+        if(h.length==0)return 0;
+        if(h.length==1)return h[0];
+        Stack<Integer> st =new Stack<>();
+        int max=0;
+        for(int i=0;i<=h.length;i++){
+            while((!st.isEmpty()) && (i==h.length || h[st.peek()]>=h[i])){
+                int h1=h[st.pop()];
+                int l=st.isEmpty()?-1:st.peek();
+                max=Math.max(max,(i-1-l)*h1);
+            }
+            st.push(i);
+        }
+        return max;
+    }
+}
