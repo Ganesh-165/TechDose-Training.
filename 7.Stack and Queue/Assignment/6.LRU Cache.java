0e49017ac60441d71eca58393895013419b13938
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2022-10-30 11:42:46 -0400

    Update 6.LRU Cache.java

--- 7.Stack and Queue/Assignment/6.LRU Cache.java
+++ 7.Stack and Queue/Assignment/6.LRU Cache.java
@@ -1,1 +1,36 @@
+class LRUCache {
 
+    int cap;
+    Map<Integer,Integer> map=new LinkedHashMap<>();
+    Queue<Integer> q =new ArrayDeque<>();
+    public LRUCache(int capacity) {
+        cap=capacity;
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            q.remove(key);
+            q.offer(key);
+            return map.get(key);
+        }
+        else return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            q.remove(key);
+            q.offer(key);
+            map.put(key,value);
+        }
+        else{
+            if(map.size()<cap){
+                q.offer(key);
+            }
+            else{
+                map.remove(q.poll());
+                q.offer(key);
+            }
+            map.put(key,value);
+        }
+    }
+}
